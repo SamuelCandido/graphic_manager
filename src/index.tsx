f19de73f91f32f69d0d3b33ffb1f50d9150c405f
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2023-03-27 14:54:01 -0300

    Pequeno erro de importação

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import { App } from './app/App';
-=======
-import { App } from './App';
->>>>>>> d6eed5db1f1df5ce95e27ec3ea8e6ee49835a3dc
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
