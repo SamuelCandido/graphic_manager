52e50ec4b785f4e6e4b38a882186c5be1afe7c69
SamuelCandido <samueljosecandido@gmail.com>
2023-03-30 11:41:46 -0300

    Inicio de um form

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -2,13 +2,19 @@
 // Componente-pagina é aquilo que retorna um html
 // Componente é aquilo que é apenas uma função
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+    const Navigate = useNavigate();
+    const handleClick = () => {
+        Navigate("/login");
+    }
     return (
         <div>
             <p>Dashboard</p>
-            <Link to="/home"></Link>
+            <button type="button" onClick={handleClick}>
+                Login
+            </button>
         </div>
     );
 }
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,15 +1,48 @@
+// O state serve para salvar o email e a senha para renderização do react
+// Dando suporte para guardar as values
 import { useNavigate } from "react-router-dom";
+
+import { useState } from "react";
 
 export const Login = () => {
   const Navigate = useNavigate();
+
   const handleClick = () => {
     Navigate("/home");
   }
+
+  const handleEntrar = () => {
+    Navigate("/home");
+  }
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <button type="button" onClick={handleClick}>
-        Go home
-      </button>
+
+      <form>
+        <label>
+          <span>Email:</span>
+          <input value={email} onChange={e => setEmail(e.target.value)} />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Login
+        </button>
+
+
+
+        <button type="button" onClick={handleClick}>
+          Go home
+        </button>
+      </form>
+
     </div>
   );
 }
